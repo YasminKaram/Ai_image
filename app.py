99f94c24b230ee273a485e994ee16b526fad1395
yasmeenKaram <yasmeenkarm4@gmail.com>
2024-05-30 01:40:49 +0300

    add

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ import base64
 from  Bi_image import bicep_image
 from  jump_image import jump_image 
 from  squat_Image import squat_image 
-from  YogaF import yoga_image 
+#from  YogaF import yoga_image 
 
 
 app = Flask(__name__)
 app.register_blueprint(bicep_image)
 app.register_blueprint(jump_image)
 app.register_blueprint(squat_image)
-app.register_blueprint(yoga_image)
+#app.register_blueprint(yoga_image)
 @app.route('/',methods=['POST'])
 def hello():
     return "FITIFY APP"
